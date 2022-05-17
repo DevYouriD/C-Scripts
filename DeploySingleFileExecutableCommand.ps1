e773eadbd7bd8eb75272f3d8555dd7821ba9983f
DevYouriD <youri.dera@gmail.com>
2022-05-17 21:09:27 +0200

    Rename DeploySingleFileExecutableCommand.ps1 to CompileToSingleExecutableFile.ps1

--- DeploySingleFileExecutableCommand.ps1
+++
@@ -1,2 +0,0 @@
-# CHANGE {USER} TO CURRENT USER AND PASTE INTO VISUAL STUDIO TERMINAL
-dotnet publish -r win-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --output "c:\users\{USER}\desktop"
