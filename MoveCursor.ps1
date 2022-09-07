5446f0be7ca3c2ad4e34604c761b9c1576aac1f4
DevYouriD <youri.dera@gmail.com>
2022-09-07 09:58:56 +0200

    Add files via upload

---
+++ MoveCursor.ps1
@@ -0,0 +1,12 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+    while($true) {
+    $position = [System.Windows.Forms.Cursor]::Position
+    $position.X++
+    [System.Windows.Forms.Cursor]::Position = $position
+
+    Write-Host "I like to move it move it"
+
+    #Duration between mouse move
+    Start-Sleep -Seconds 60
+    }
